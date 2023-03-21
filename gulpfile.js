f305bc8eea97d63d212f723f476093c81a713ef4
wangchunsheng <chunsheng.wang@ys100.com>
2023-03-21 09:54:40 +0800

    wcs 编译scss文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 const gulp = require('gulp')
 // const less = require('gulp-less') // less 编译成 css
-// const sass = require('gulp-sass')(require('sass')) //  sass 编译成 css 高版本sass使用方法
-const sass = require('gulp-sass') //  sass 编译成 css
+// const scss = require('gulp-sass')(require('sass')) //  scss 编译成 css 高版本scss使用方法
+const scss = require('gulp-sass') //  scss 编译成 css
 const cleancss = require('gulp-clean-css') //文件压缩
 const rename = require('gulp-rename') //文件更名
 
-//sass编译任务
+//scss编译任务
 gulp.task('type-a', () => {
   return gulp
-    .src('./public/sass/themes/type-a/*.sass')
-    .pipe(sass())
+    .src('./public/css-preprocessors/scss/themes/type-a/*.scss')
+    .pipe(scss())
     .pipe(gulp.dest('./public/css/themes/type-a'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(cleancss()) //压缩
@@ -18,8 +18,8 @@ gulp.task('type-a', () => {
 
 gulp.task('type-b', () => {
   return gulp
-    .src('./public/sass/themes/type-b/*.sass')
-    .pipe(sass())
+    .src('./public/css-preprocessors/scss/themes/type-b/*.scss')
+    .pipe(scss())
     .pipe(gulp.dest('./public/css/themes/type-b'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(cleancss()) //压缩
@@ -28,8 +28,8 @@ gulp.task('type-b', () => {
 
 gulp.task('type-c', () => {
   return gulp
-    .src('./public/sass/themes/type-c/*.sass')
-    .pipe(sass())
+    .src('./public/css-preprocessors/scss/themes/type-c/*.scss')
+    .pipe(scss())
     .pipe(gulp.dest('./public/css/themes/type-c'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(cleancss()) //压缩
@@ -38,8 +38,8 @@ gulp.task('type-c', () => {
 
 gulp.task('type-d', () => {
   return gulp
-    .src('./public/sass/themes/type-d/*.sass')
-    .pipe(sass())
+    .src('./public/css-preprocessors/scss/themes/type-d/*.scss')
+    .pipe(scss())
     .pipe(gulp.dest('./public/css/themes/type-d'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(cleancss()) //压缩
@@ -48,8 +48,8 @@ gulp.task('type-d', () => {
 
 gulp.task('type-e', () => {
   return gulp
-    .src('./public/sass/themes/type-e/*.sass')
-    .pipe(sass())
+    .src('./public/css-preprocessors/scss/themes/type-e/*.scss')
+    .pipe(scss())
     .pipe(gulp.dest('./public/css/themes/type-e'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(cleancss()) //压缩
@@ -58,8 +58,8 @@ gulp.task('type-e', () => {
 
 gulp.task('type-full', () => {
   return gulp
-    .src('./public/sass/themes/type-full/*.sass')
-    .pipe(sass())
+    .src('./public/css-preprocessors/scss/themes/type-full/*.scss')
+    .pipe(scss())
     .pipe(gulp.dest('./public/css/themes/type-full'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(cleancss()) //压缩
@@ -68,8 +68,8 @@ gulp.task('type-full', () => {
 
 gulp.task('nifty', () => {
   return gulp
-    .src('./public/sass/nifty.sass')
-    .pipe(sass())
+    .src('./public/css-preprocessors/scss/nifty.scss')
+    .pipe(scss())
     .pipe(gulp.dest('./public/css/nifty'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(cleancss()) //压缩
